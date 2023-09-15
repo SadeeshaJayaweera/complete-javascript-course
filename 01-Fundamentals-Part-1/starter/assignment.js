0263198a6584d94784a8a19a06a7ea8aef632020
Sadeesha_Jayaweera <132116738+SadeeshaJayaweera@users.noreply.github.com>
2023-09-15 19:09:44 +0530

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,33 @@
+// Assignment Answers for JavaScript Fundamentals - Part 1 
+
+// Lecture - Values and Variables - Questions 
+let country = "Sri Lanka";
+let continent ="Asia Pacific";
+let population =20;
+
+console.log("My Country is: ",country);
+console.log("My Continent is: ",continent);
+console.log("Population is: ",population);
+
+
+// Lecture - Data Types - Questions 
+let isIsland = true;
+const language = "sinhala" // let language; - this is a undefined data type 
+
+//how to check a type of data type
+console.log(typeof "sinhala");
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+console.log("Is a Island: ",isIsland);
+console.log("My Country is: ",country);
+console.log("Population is: ", population);
+console.log("The Language is: ",language);
+
+//Lecture - let,const and var 
+population = 21 ; //changed the population to 21 million which is defined as inital to 20 milliom above 
+console.log(population);
+
+// Lecture - Basic Operations 
+
