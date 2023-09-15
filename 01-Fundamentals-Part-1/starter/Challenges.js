b596a42d639f1eddf62b6196baf04cc4b21e5e7a
Sadeesha_Jayaweera <132116738+SadeeshaJayaweera@users.noreply.github.com>
2023-09-15 22:11:23 +0530

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/Challenges.js
@@ -0,0 +1,34 @@
+//This File Contains the Challenges Answers for this Course 
+
+//Challeng - 1
+
+let massMark = 78;
+let heightMark = 1.69;
+
+let massJohn = 92;
+let heightJohn = 1.93;
+
+let BMIMark = (78/(1.69*1.69));
+let BMIJohn = (92/(1.93*1.93));
+
+console.log(BMIMark);
+console.log(BMIJohn);
+
+//bonus 
+let markHigherBMI =(BMIMark>BMIJohn);
+console.log(markHigherBMI);
+
+
+//Challenge 2 
+
+if(BMIJohn>BMIMark)
+{
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
+
+else{
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+}
+
+//Challemge 3 
+
